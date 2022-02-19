0493a2d233a5aa77423704080198e6f915fb6f56
blautista <bautistaromanazzi@gmail.com>
2022-02-19 19:30:09 -0300

    Fixed pages folder structure issue

---
+++ src/pages/BirdDetails.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const BirdDetails = (props: Props) => {
+  return <div>BirdDetails</div>;
+};
+
+export default BirdDetails;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const MainPage = (props: Props) => {
+  return <div>MainPage</div>;
+};
+
+export default MainPage;
