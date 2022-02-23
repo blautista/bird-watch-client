d684acfdd338325004d572c157d7e801fd298f26
blautista <bautistaromanazzi@gmail.com>
2022-02-23 16:03:59 -0300

    Created MainLayout component

---
+++ src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+const MainLayout: React.FC = ({ children }) => {
+  return <Grid>{children}</Grid>;
+};
+
+export default MainLayout;
