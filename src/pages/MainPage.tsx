ef87ed5dc9e6a847d4e0f0a1d77075bc31edc5ca
blautista <bautistaromanazzi@gmail.com>
2022-02-23 20:23:53 -0300

    Bird fetch dispatch is now handled on MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,5 @@
-import { Grid, Button } from "@mui/material";
-import React from "react";
-import dummyData from "../components/BirdCardList/dummyData.json";
+import React, { useEffect, useState } from "react";
+import { Grid, CircularProgress, Typography } from "@mui/material";
 import {
   BirdDetailsHeader,
   BirdDetailsHeaderTitle,
@@ -12,23 +11,31 @@ import {
   selectBirdsArray,
   fetchBirdsAsync,
   selectUserLocation,
+  selectFetchStatus,
 } from "../features/birds/birdSlice";
 
 type Props = {};
 
 const MainPage = (props: Props) => {
-  const handleButtonClick = () => {
-    navigator.geolocation.getCurrentPosition(
-      ({ coords: { latitude, longitude } }) => {
-        dispatch(fetchBirdsAsync({ lat: latitude, lng: longitude }));
-      }
-    );
-  };
-
   const birdsArray = useAppSelector(selectBirdsArray);
+  const isLoading = useAppSelector(selectFetchStatus);
   const userLocation = useAppSelector(selectUserLocation);
-  console.log("i fetched deta");
+  const [error, setError] = useState<string | null>(null);
+
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!birdsArray) {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords: { latitude, longitude } }) => {
+          dispatch(fetchBirdsAsync({ lat: latitude, lng: longitude }));
+        },
+        () => {
+          setError("The app needs location permissions to fetch birds");
+        }
+      );
+    }
+  }, []);
 
   return (
     <Grid container spacing={2} justifyContent="center">
@@ -42,13 +49,20 @@ const MainPage = (props: Props) => {
         )}
       </BirdDetailsHeader>
       <Grid item maxWidth={1200} m={1}>
-        {birdsArray && birdsArray.length > 0 && (
+        {birdsArray ? (
           <BirdCardList birdsArray={birdsArray} />
+        ) : isLoading === "loading" ? (
+          <>
+            <CircularProgress />
+            <Typography variant="h5">Fetching birds near you...</Typography>
+          </>
+        ) : (
+          <Typography variant="h5">
+            There was a problem fetching the birds. Please refresh the page and
+            try again.
+          </Typography>
         )}
-        {!birdsArray ||
-          (birdsArray.length === 0 && <p>click the button bro</p>)}
       </Grid>
-      <Button onClick={handleButtonClick}>gimme birds</Button>
     </Grid>
   );
 };
