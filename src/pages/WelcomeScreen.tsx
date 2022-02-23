830fa518ea51d226c3fce84e12eb991afe744560
blautista <bautistaromanazzi@gmail.com>
2022-02-23 20:53:05 -0300

    Updated WelcomeScreen styling

--- src/pages/WelcomeScreen.tsx
+++ src/pages/WelcomeScreen.tsx
@@ -2,8 +2,8 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
-import { Grid, Typography, Button, Link } from "@mui/material";
-import { css, jsx } from "@emotion/react";
+import { Grid, Typography, Button, Link, TextField } from "@mui/material";
+import { css } from "@emotion/react";
 
 const WelcomeScreen: React.FC = () => {
   const navigate = useNavigate();
@@ -19,25 +19,43 @@ const WelcomeScreen: React.FC = () => {
       justifyContent="space-around"
       alignItems="center"
       height="100vh"
-      css={css`
-        background-image: url("bg.jpg");
-      `}
     >
-      <Grid item>
+      <Grid item spacing={2}>
         <Typography variant="h1">Bird Watch App</Typography>
+        <Typography variant="h5">
+          I will give you the latest notable bird sightings near your area.
+        </Typography>
       </Grid>
-      <Grid item>
-        <Button
-          onClick={handleWelcomeScreenClick}
-          variant="contained"
-          css={css`
-            width: 300px;
-            height: 150px;
-            font-size: 2rem;
-          `}
-        >
-          See birds near me
-        </Button>
+      <Grid
+        item
+        container
+        justifyContent="center"
+        alignItems="center"
+        direction="row"
+      >
+        <Grid item>
+          <Button
+            onClick={handleWelcomeScreenClick}
+            size="large"
+            variant="contained"
+          >
+            Use my current location
+          </Button>
+        </Grid>
+        {/* <Grid item container direction="column" spacing={3}>
+          <Grid item>
+            <Typography variant="h6">
+              Or, alternatively, input the coordinates below:
+            </Typography>
+            <TextField variant="filled" label="Latitude"></TextField>
+            <TextField variant="filled" label="Longitude"></TextField>
+          </Grid>
+          <Grid item>
+            <Button onClick={handleWelcomeScreenClick} variant="contained">
+              Give me birds!
+            </Button>
+          </Grid>
+        </Grid> */}
       </Grid>
     </Grid>
   );
